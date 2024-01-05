98fc44a6c0eb20a84f397b3ffa0b67b7dad30654
Kapazo <153466200+Kapazo@users.noreply.github.com>
2024-01-05 14:44:51 -0300

    Update1Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 namespace DesafioPOO.Models
 {
  
+  public class Nokia : Smartphone
+   {
     public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
             
