62d3d4a73efce598206868ca98c262b12e472e6a
Kapazo <153466200+Kapazo@users.noreply.github.com>
2024-01-05 14:43:58 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 namespace DesafioPOO.Models
 {
  
-    public class Nokia : Smartphone
+    public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
             
-        } 
+        }  
    
     public override void InstalarAplicativo(string NomeApp )
        {
